beae2e34a593f69f458d0c3c90bc21fa439df5ef
Lydia <lreitzel@gmail.com>
2021-08-05 01:26:42 -0400

    commit tree 5222c2a7964a696a3a319d09439dcd17cff7b872

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(src, callback) {
+    const r = [];
+  
+    for (let i = 0; i < src.length; i++) {
+      const theElement = src[i];
+      r.push(callback(theElement));
+    }
+  
+    return r;
+}
+
+function reduce(arr, callback, initValue) {
+    if (initValue) {
+        let total = initValue
+        for (let i = 0; i < arr.length; i++) {
+            total = callback(arr[i], total)
+        }
+        return total;
+    } else {
+        let total = arr[0]
+        for (let i = 1; i < arr.length; i++) {
+            total = callback(arr[i], total)
+        }
+        return total;
+    }
+}
